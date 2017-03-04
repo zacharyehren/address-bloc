e74d3c56c05c072b63f19057492f630ebef9d8b0
Zachary Ehren <zachehren@gmail.com>
2017-03-03 16:17:29 -0800

    create menu_controller

---
+++ controllers/menu_controller.rb
@@ -0,0 +1,22 @@
+require_relative '../models/address_book'
+
+class MenuController
+  attr_reader :address_book
+
+  def initialize
+    @address_book = AddressBook.new
+  end
+
+  def main_menu
+    puts "Main Menu - #{address_book.entries.count} entries"
+    puts "1 - View all entries"
+    puts "2 - Create an entry"
+    puts "3 - Search for an entry"
+    puts "4 - Import entries from a CSV"
+    puts "5 - Exit"
+    print "Enter your selection: "
+
+    selection = gets.to_i
+    puts "You picked #{selection}"
+  end
+end
