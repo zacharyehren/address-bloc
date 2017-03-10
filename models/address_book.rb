ee4073e9efe48ef5e25d84927e623fd00c3608d5
Zachary Ehren <zachehren@gmail.com>
2017-03-09 20:30:56 -0800

    assignment 8 complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -77,4 +77,13 @@ class AddressBook
 
   end
 
+  def iterative_search(name)
+    entries.each do |entry|
+      if entry.name == name
+        return entry
+      end
+  end
+  return nil
+end
+
 end
